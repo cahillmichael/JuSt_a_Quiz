355916960ed33d37e8e96ecf5c12b3700a6122a0
michaelc <michaelc@carimus.com>
2022-03-07 02:27:59 -0500

    added questions  and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
-var startBtn = window.document.querySelector("#start-btn");
-var quizBox = window.document.querySelector("#quiz-box");
+var startBtn = document.querySelector("#start-btn");
+var quizBox = document.querySelector("#quiz-box");
+var qCount = 0;
 
 var allQuestions = [
     {
@@ -61,9 +62,47 @@ var clearQuizBox = function() {
 
 //start quiz
 var startQuiz = function() {
-    console.log("clicked");
-    clearQuizBox();
 
+    clearQuizBox();
+    newQuestion();
+}
+
+//load new question data
+var newQuestion = function() {
+    if(qCount >= allQuestions.length){
+        clearQuizBox();
+    }
+    else{
+        clearQuizBox();
+        
+        var questionText = document.createElement("h2");
+        questionText.className = "questionHeader";
+        questionText.textContent = allQuestions[qCount].question;
+
+        var answers = document.createElement("div");
+        answers.className = "answers";
+        var answer1 = document.createElement("button");
+        var answer2 = document.createElement("button");
+        var answer3 = document.createElement("button");
+        var answer4 = document.createElement("button");
+
+        answer1.className = "answer-btn";
+        answer2.className = "answer-btn";
+        answer3.className = "answer-btn";
+        answer4.className = "answer-btn";
+
+        answer1.textContent = "1. " + allQuestions[qCount].answers[1];
+        answer2.textContent = "2. " + allQuestions[qCount].answers[2];
+        answer3.textContent = "3. " + allQuestions[qCount].answers[3];
+        answer4.textContent = "4. " + allQuestions[qCount].answers[4];
+
+        quizBox.appendChild(questionText);
+        quizBox.appendChild(answers);
+        answers.appendChild(answer1);
+        answers.appendChild(answer2);
+        answers.appendChild(answer3);
+        answers.appendChild(answer4);
+    }
 }
 
 startBtn.addEventListener("click", startQuiz);
